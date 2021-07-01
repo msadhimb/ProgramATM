1297ae5e0530bec4eaa792b4bd6ca25441e8814a
msadhimb <86710473+msadhimb@users.noreply.github.com>
2021-07-01 14:31:19 +0700

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,5 +1,11 @@
 from getpass import getpass
-from flask import Flask
+import os
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 
 temp = [0]
